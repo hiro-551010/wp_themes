1c05704ccc29cb2dc092f0cf75306d76d2b046c4
hiro-551010 <hiro551010@gmail.com>
2021-07-06 12:23:29 +0900

    firstcommit

---
+++ test/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/style.css">
+    <title><?php bloginfo('name');?></title>
+</head>
+<body>
+
+</body>
+</html>
